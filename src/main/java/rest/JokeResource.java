17323bc5ce8f147c33383bdf13ba8062f1ad7463
MartinBrandstrup <martin.l.brandstrup@gmail.com>
2020-06-09 09:01:36 +0200

    Update JokeResource.java
    Added scraping methods

--- src/main/java/rest/JokeResource.java
+++ src/main/java/rest/JokeResource.java
@@ -1,10 +1,17 @@
 package rest;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import dtos.ChuckDTO;
 import dtos.DadDTO;
 import dtos.OurDTO;
+import facades.FacadeExample;
 import java.io.IOException;
+import java.net.ProtocolException;
+import java.net.SocketTimeoutException;
+import java.util.HashMap;
+import javax.persistence.EntityManagerFactory;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
@@ -13,47 +20,112 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.core.MediaType;
-import utils.HttpUtils;
+import utils.EMF_Creator;
+import utils.HttpUtils_deprecated;
+import utils.RequestSender;
 
 /**
  * REST Web Service
  *
- * @author Christian
+ * @author Brandstrup
  */
 @Path("jokes")
-public class JokeResource {
+public class JokeResource
+{
 
     @Context
     private UriInfo context;
 
-   
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public String getJokes() throws IOException {
-       Gson gson = new Gson();
-        String chuck1 = HttpUtils.fetchData("https://api.chucknorris.io/jokes/random");
-        ChuckDTO chuckDTO1 = gson.fromJson(chuck1, ChuckDTO.class);
-        
-        String chuck2 = HttpUtils.fetchData("https://api.chucknorris.io/jokes/random");
-        ChuckDTO chuckDTO2 = gson.fromJson(chuck2, ChuckDTO.class);
-        
-        String chuck3 = HttpUtils.fetchData("https://api.chucknorris.io/jokes/random");
-        ChuckDTO chuckDTO3 = gson.fromJson(chuck3, ChuckDTO.class);
-        
-        String chuck4 = HttpUtils.fetchData("https://api.chucknorris.io/jokes/random");
-        ChuckDTO chuckDTO4 = gson.fromJson(chuck4, ChuckDTO.class);
+    private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory(
+            "pu",
+            "jdbc:mysql://localhost:3307/2020S3P_test",
+            "dev",
+            "ax2",
+            EMF_Creator.Strategy.CREATE);
 
-        String dad = HttpUtils.fetchData("https://icanhazdadjoke.com");
-        DadDTO dadDTO = gson.fromJson(dad, DadDTO.class);
+    private static final FacadeExample FACADE = FacadeExample.getFacadeExample(EMF);
+    private static final RequestSender requestSender = RequestSender.getRequestSender();
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    
+    // URLs
+    private static final String dadURL = "https://icanhazdadjoke.com";
+    private static final String chuckURL = "https://api.chucknorris.io/jokes/random";
+    
 
-        OurDTO combinedDTO = new OurDTO(chuckDTO1, chuckDTO2, chuckDTO3, chuckDTO4, dadDTO);
+    private ChuckDTO scrapeChuckJoke(String url)
+            throws SocketTimeoutException, ProtocolException, IOException
+    {
+        Gson gson = new Gson();
+        HashMap headers = new HashMap();
+        headers.put("Accept", "application/json;charset=UTF-8");
+        headers.put("User-Agent", "server");
+        String result = requestSender.sendRequest(url, "GET", headers, 5000);
 
-        //This is what your endpoint should return
-        String combinedJSON = gson.toJson(combinedDTO);
+        // Jeg mener at TypeToken er den, der holder styr på hvilke JSON attributter
+        // der bliver smidt med og får dem placeret korrekt i den resulterende DTO
+        // så længe at navngivningen holder
+        ChuckDTO chuckDTO = gson.fromJson(result, new TypeToken<ChuckDTO>()
+        {
+        }.getType());
 
-
-        return combinedJSON;
+        return chuckDTO;
     }
 
-   
+    private DadDTO scrapeDadJoke(String url)
+            throws SocketTimeoutException, ProtocolException, IOException
+    {
+        Gson gson = new Gson();
+        HashMap headers = new HashMap();
+        headers.put("Accept", "application/json;charset=UTF-8");
+        headers.put("User-Agent", "server");
+        String result = requestSender.sendRequest(url, "GET", headers, 5000);
+
+        DadDTO dadDTO = gson.fromJson(result, new TypeToken<DadDTO>()
+        {
+        }.getType());
+
+        return dadDTO;
+    }
+
+    @GET
+    @Path("/{chuck}/{dad}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getJokes()
+    {
+        Gson gson = new Gson();
+
+        CombinedDTO combDTO = new CombinedDTO();
+        
+        String combJSON = gson.toJson(combDTO);
+        return combJSON;
+    }
+
+//    @GET
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public String getJokes() throws IOException
+//    {
+//        Gson gson = new Gson();
+//        String chuck1 = HttpUtils_deprecated.fetchData("https://api.chucknorris.io/jokes/random");
+//        ChuckDTO chuckDTO1 = gson.fromJson(chuck1, ChuckDTO.class);
+//
+//        String chuck2 = HttpUtils_deprecated.fetchData("https://api.chucknorris.io/jokes/random");
+//        ChuckDTO chuckDTO2 = gson.fromJson(chuck2, ChuckDTO.class);
+//
+//        String chuck3 = HttpUtils_deprecated.fetchData("https://api.chucknorris.io/jokes/random");
+//        ChuckDTO chuckDTO3 = gson.fromJson(chuck3, ChuckDTO.class);
+//
+//        String chuck4 = HttpUtils_deprecated.fetchData("https://api.chucknorris.io/jokes/random");
+//        ChuckDTO chuckDTO4 = gson.fromJson(chuck4, ChuckDTO.class);
+//
+//        String dad = HttpUtils_deprecated.fetchData("https://icanhazdadjoke.com");
+//        DadDTO dadDTO = gson.fromJson(dad, DadDTO.class);
+//
+//        OurDTO combinedDTO = new OurDTO(chuckDTO1, chuckDTO2, chuckDTO3, chuckDTO4, dadDTO);
+//
+//        //This is what your endpoint should return
+//        String combinedJSON = gson.toJson(combinedDTO);
+//
+//        return combinedJSON;
+//    }
+
 }
